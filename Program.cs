438abc2c3ac822033265e0cce9fd203b8996b5e1
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-26 23:07:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read(); // Ekrana gelir ve enter basana kadar kalır
+        }
+    }
+}
