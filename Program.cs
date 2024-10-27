57873eccb513376899059c264a814ec9fda2ecc0
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-27 15:38:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = 22;
+
+            int number1, number2, sum;
+            number1 = 1;
+            number2 = 1;
+            sum = number1 + number2;
+
+            Console.WriteLine(number);
+            Console.Write(sum);
+
+            Console.Read(); 
+        }
+    }
+}
