1013654115a328921537ad157e9a532702f431f6
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 21:40:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username, password;
+            Console.Write("Username: ");
+            username = Console.ReadLine();
+            Console.WriteLine("Password: ");
+            password = Console.ReadLine();
+
+            if(username == "Admin" & password == "123456")
+            {
+                Console.Write("True");
+            }
+            else
+            {
+                Console.Write("False");
+            }
+
+
+            Console.Read();
+        }
+    }
+} 
