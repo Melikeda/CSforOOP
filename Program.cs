ce455072b64b59e220b1ccc13083968912429afc
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 13:45:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.Write("Enter the number: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write(n1);
+
+            Console.Read();
+        }
+    }
+} 
