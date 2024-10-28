9b35186e4daf12e3054dec00f41da0006f9eba18
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 13:32:36 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, sum;
+            Console.Write("Enter the n1: ");
+            n1 = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Enter the n2: ");
+            n2 = Convert.ToInt16(Console.ReadLine());
+            sum = n1 + n2;
+            Console.Write(sum);
+
+            Console.Read();
+        }
+    }
+} 
