bd030975244946cdbad30371cd2bde0e3b658cdb
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 21:17:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool situation;
+            situation = true;
+            Console.Write("Did you pass the exam? ");
+            Console.Write(situation);
+
+            Console.Read();
+        }
+    }
+} 
