6b41bce565084fea0f006932ae73a24f470d89b5
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 13:28:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter the number: ");
+            number = Convert.ToInt16(Console.ReadLine());  //Klavyeden girilen değerler stribg türünde olduğu için bunu int türüne çevirmeliyiz
+            Console.Write(number);
+
+            Console.Read();
+        }
+    }
+} 
