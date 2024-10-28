c025b6321facdd76451c2e5e985ed22b5edd9f66
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 21:29:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city;
+            Console.Write("City name: ");
+            city = Console.ReadLine();
+
+            if(city == "Erzurum")
+            {
+                Console.Write("True city");
+            }
+            else
+            {
+                Console.Write("False city");
+            }
+
+
+            Console.Read();
+        }
+    }
+} 
