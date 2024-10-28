ae45eddcb04a3d8c10a95d872a6fa32fe03c2460
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-28 13:20:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country, city;
+            Console.Write("Please enter the country: ");
+            country = Console.ReadLine(); //Klavyeden alınan değerin hafıza tutulması
+            Console.Write("Please enter the city: ");
+            city = Console.ReadLine();
+            Console.WriteLine(country + " " + city);
+
+            Console.Read();
+        }
+    }
+} 
