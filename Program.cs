7d526ec189b1a85fdfc2b6ab219366f7220eea90
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-29 17:34:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.Write("Enter the number: ");
+            number = Convert.ToInt16(Console.ReadLine());
+
+            for(int i=1; i<=number; i++)
+            {
+                if (number % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.Read();
+        }
+
+    }
+}
