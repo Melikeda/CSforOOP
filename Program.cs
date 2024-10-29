ec0b3d13d9d7d67ec1dc666e0335714d96e303e4
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-29 17:03:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte plate;
+            Console.Write("Enter the license plate: ");
+            plate = byte.Parse(Console.ReadLine());
+
+            switch (plate)
+            {
+                case 19:
+                    Console.WriteLine("Hello Çorum");
+                    break;
+                case 34:
+                    Console.WriteLine("Hi İstanbul");
+                        break;
+                case 35:
+                    Console.WriteLine("Hi There İzmir");
+                    break;
+                default:
+                    Console.WriteLine("There is no this city");
+                    break;
+
+            }
+
+            Console.Read();
+        }
+
+    }
+}
