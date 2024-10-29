d05e09e66a053de30adeaa815127eb305e3307ce
Melike Külahcı <m.edakulahci@gmail.com>
2024-10-29 17:17:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Hello C#");
+            }
+
+
+            Console.Read();
+        }
+
+    }
+}
