501b0c925d2b0bcf2c97670f65afa07233b00d37
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-01 17:44:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] city = new string[5]; //Klavyeden veri girişi olduğunda
+
+            for(int i= 0; i < 5; i++)
+            {
+                Console.Write(" City name: ");
+                city[i] = Console.ReadLine();
+            }
+
+            for(int j = 0; j < 5; j++)
+            {
+                Console.WriteLine(city[j]);
+            }
+
+            Console.Read();
+        }
+    }
+}
