4c93933b481e3992b372a53ed5bf43089098fdf8
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-01 14:51:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 10, 20, 30, 40, 50, 60, 70 };
+            int sum = 0;
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                sum = sum + number[i];
+               // Console.WriteLine(sum);
+            }
+
+            Console.WriteLine(sum);
+
+            Console.Read();
+        }
+    }
+}
