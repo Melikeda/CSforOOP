5b374a185e67fc993bcb2387538aca77b7c70d90
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-01 21:23:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        public static void sumMethod()   
+        {
+            int n1, n2, sum;
+            n1 = 35;
+            n2 = 3;
+            sum = n1 + n2;
+            
+           Console.WriteLine(sum);
+        }
+
+
+        static void Main(string[] args)
+        {
+            sumMethod();
+            
+        }
+    }
+}
