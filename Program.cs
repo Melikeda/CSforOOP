15f222a09592b143e127101a90db90c3946cb6bd
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-01 14:58:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = { 120, 20, 0, 40, 660, 7 };
+            Array.Sort(number);
+
+            for (int i = 0; i <number.Length; i++)
+            {
+                Console.WriteLine(number[i]);
+            }
+
+            Console.Read();
+        }
+    }
+}
