ac681fbf1e84bc481dc9115f52f0756a2ce67c53
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-02 19:36:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PeopleClass people = new PeopleClass();
+            people.ad = "Melike";
+            people.soyad = "Külahcı";
+            people.id = 1;
+            people.sehir = "Düzce";
+
+
+            Console.WriteLine("Ad: " + people.ad + " Soyad: " + people.soyad + " id: " + people.id + " Şehir: " + people.sehir);
+            
+            Console.ReadLine();
+
+        }
+
+    }
+}
