09875637b8c514d651596e6894593664b6a4c295
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-02 20:01:24 +0300

    Add files via upload

---
+++ Kapsülleme2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    internal class PeopleClass
+    {
+        public string ad;
+        public string soyad;
+        public int id;
+        public string sehir;
+
+
+        public string Ad { get => ad; set => ad = value.ToLower(); }  //BU bir property 
+        public string Soyad  // soyas Soyad olamlı yani yukarıdaki gibi aynı yazma 
+        { 
+            get => soyad; 
+            set => soyad = value.ToUpper(); 
+        }
+    }
+}
