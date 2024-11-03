8ac2d75f0c3fb20289586ce53a80c6f95e1a9e38
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-03 15:13:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double number;
+            Console.Write("Enter the number: ");
+            number = Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Mutlak Değer: " + Math.Abs(number));
+
+            Console.ReadLine();
+        }
+
+    }
+}
