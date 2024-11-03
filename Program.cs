0f362b060c2298c8eef9e3b5518a0df8eb504254
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-03 15:22:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string metin1, metin2;
+            Console.Write("Enter the metin1: ");
+            metin1 = Console.ReadLine();
+            Console.Write("Enter the metin2: ");
+            metin2 = Console.ReadLine();
+            Console.WriteLine("Concat ile birleştirme: " + string.Concat(metin1, metin2));
+
+            Console.ReadLine();
+        }
+
+    }
+}
