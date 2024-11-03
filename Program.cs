03cd031df8f032589ce79ca16b69da81352edd8f
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-03 15:47:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number;
+            Random ran = new Random();
+            number = ran.Next(0, 51);  //Birinci sayı random yazılabilir ama ikinci say yazılmaz
+
+            Console.WriteLine(number);
+
+            Console.ReadLine();
+        }
+
+    }
+}
