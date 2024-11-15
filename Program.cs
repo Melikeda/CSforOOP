79c0541f75d78d680b0e6fed9a0dcd947db4cc68
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-15 22:26:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldCS
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int number1, number2, sum;
+
+            Console.WriteLine("Enter the first number: ");
+            number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the first number: ");
+            number2 = int.Parse(Console.ReadLine());
+
+            sum = number1 + number2;
+            Console.WriteLine("Sum is" + " "+ sum); //
+            Console.WriteLine();
+            Console.WriteLine($"Sum is {sum}"); // string interpolation
+
+        }
+
+    }
+}
