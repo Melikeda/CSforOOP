62107796bf63db9274cc1d54fbb2f3414e8ec7b2
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-16 18:34:41 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Cache;
+
+namespace ConsoleApp
+{
+    class Person
+    {
+        private int age;
+
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetAge(int newAge)
+        {
+            age = newAge;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myPerson = new Person();
+
+            myPerson.SetAge(23);
+
+            Console.WriteLine($"myPerson's age is: {myPerson.GetAge()}");
+
+        }
+    }
+}
