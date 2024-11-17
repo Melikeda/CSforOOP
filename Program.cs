f09f6481f4493e589dafaa06a624c8c15fb53802
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-17 14:05:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Studentcs
+{
+
+    class Account
+    {
+        private string name;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main() 
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is: {myAccount.Name}");
+
+            Console.WriteLine("Enter the name: ");
+            string thename = Console.ReadLine();
+
+        
+            myAccount.Name = thename;
+
+           
+            Console.WriteLine($"myAccount's name is: {myAccount.Name}");
+
+        }
+    }
+
+}
