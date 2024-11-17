8553b9d1d04a4703675883c719a25fd7a9908630
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-17 13:37:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Studentcs
+{
+
+    class Program
+    {
+        static void Main()  // Klavyeden girilen 10 notun ortalamasının bulunması
+        {
+            int total = 0;
+            int gradeCounter = 1;
+            int average;
+
+            while (gradeCounter <= 10)
+            {
+                Console.WriteLine("Enter grade");
+                int grade = int.Parse(Console.ReadLine());
+                total = total + gradeCounter;
+                gradeCounter = gradeCounter + 1; 
+            }
+
+            average = total / 10;
+
+            Console.WriteLine($"Total all of 10 grades is {total}");
+            Console.WriteLine($"Class average is {average}");
+
+        }
+    }
+
+}
