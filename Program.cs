6f36c42904028167ea5a896c2f33b1ec20037fca
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-17 14:19:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Studentcs
+{
+
+    class Account
+    {
+        public string Name { get; set; }  // auto-implemented property
+
+        public Account(string accountName)
+        {
+            Name = accountName;
+        }
+
+        
+    }
+
+    class Program
+    {
+        static void Main() 
+        {
+            Account myAccount = new Account("Melike Külahcı");
+
+            Console.WriteLine($"myAccount's name is: {myAccount.Name}");
+
+        }
+    }
+
+}
