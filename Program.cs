faf67e411a4c41dbfa567073e1f1f9dcb058b6e2
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-18 17:57:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class RandomNunber
+{
+    static void Main()
+    {
+        Random myRandom = new Random();
+
+        for(int counter = 1; counter <= 20; ++counter)
+        {
+            int face = myRandom.Next(1,7);  // 7: Üst sınır (hariç tutulur, bu yüzden 6'ya kadar değer üretir).
+                                            // Rastgele bir sayı üretmek için random sınıfının next methodunu kullanırız
+            Console.Write(" " + face);
+        }
+
+        Console.WriteLine();
+    }
+}
