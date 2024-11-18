c029fd2b848fea43877920dd3231cf6016014278
Melike Külahcı <m.edakulahci@gmail.com>
2024-11-18 20:16:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class CalculatePower
+{
+    static void Main()
+    {
+        Console.WriteLine($"Power 10 = {Power(10)} ");  // Üs (exponentValue) değeri belirtilmediği için, varsayılan değer olan 2 kullanılır.
+        Console.WriteLine($"Power (2, 10) = {Power(2,10)} ");
+    }
+
+    static int Power(int baseValue, int exponentValue = 2)
+    {
+        int result = 1;  // Sonuç başlangıçta 1 olarak ayarlanır (çarpma işlemi için nötr eleman).
+
+        for (int i= 1; i <= exponentValue; ++i)  // Döngü, exponentValue kadar tekrar eder.
+        {                                        // Her tekrarda, *result = baseValue; ifadesiyle result, baseValue ile çarpılır.
+            result *= baseValue;
+        }
+
+        return result;
+    }
+}
+
+ 
