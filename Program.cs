1f6e71ffb40535d8715fa698e55d30d14d4d403d
Melike Külahcı <m.edakulahci@gmail.com>
2024-12-04 21:44:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class InitArray
+{
+    static void Main()
+    {
+        int[] array = new int[5];  // 5 elemanlı bir tamsayı dizisi tanımlanır. Tüm elemanlar varsayılan 0 olarak başlatılır.
+
+        Console.WriteLine($"{"Index"}{"Value", 8}"); // value başlığını 8 boşluk sağa hizalayarak yazar.
+
+        for (int counter = 0; counter < array.Length; ++counter)  // Döngü, dizinin uzunluğuna (Length) kadar çalışır.
+        {
+            Console.WriteLine($"{counter,5}{array[counter],8}");   // Dizinin indeksi (counter) ekrana 5 karakter genişliğinde, sağa hizalanmış şekilde yazdırılır.
+        }                                                          // Dizinin değeri (array[counter]) 8 karakter genişliğinde, sağa hizalanmış şekilde yazdırılır.
+    }
+}
