44998a1c624aa401b524ce7f1dbd824f226c9274
Melike Külahcı <m.edakulahci@gmail.com>
2024-12-04 22:35:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class InitArray
+{
+    static void Main()
+    {
+        int[] array = { 12, 23, 34, 45, 56 };
+
+        Console.WriteLine($"{"Index"}{"Value",8}");
+
+        for (int counter = 0; counter < array.Length; ++counter)
+        {
+            Console.WriteLine($"{counter,5}{array[counter],8}");
+        }
+    }
+}
