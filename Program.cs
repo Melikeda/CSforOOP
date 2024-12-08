06d64aa71ce7ef8315a834e9b0dc54e3f7644304
Melike Külahcı <m.edakulahci@gmail.com>
2024-12-08 15:22:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class RollDie
+{
+    static void Main()
+    {
+        var randomNumbers = new Random();  // var, bir değişkenin türünü açıkça belirtmek yerine, derleyiciye bu türü anlamasını söylersiniz.
+        var frequency = new int[7];
+
+        for (var roll = 1; roll <= 600000000; ++roll)
+        {
+            ++frequency[randomNumbers.Next(1, 7)];
+        }
+
+        Console.WriteLine($" {"Face"} {"Frequency", 10}");
+
+        for ( var face = 1; face < frequency.Length; ++face)
+        {
+            Console.WriteLine($" {face,4} {frequency[face],10} ");
+        }
+    }
+}
+   
+
+ 
