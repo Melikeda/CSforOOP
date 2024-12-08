b02670122cf9486711f1c99de82f2a941316e8f2
Melike Külahcı <m.edakulahci@gmail.com>
2024-12-08 15:09:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ForEachTest
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int total = 0;
+
+        foreach (int number in array)
+        {
+            total = total + number;
+        }
+
+        Console.WriteLine($"Total of array elements: {total}");
+    }
+}
+   
+
+ 
