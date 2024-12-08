88aeb9ddc0b8c85114112c076f9ef7c5a753904b
Melike Külahcı <m.edakulahci@gmail.com>
2024-12-08 16:02:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+
+class LinqTest
+{
+    static void Main()
+    {
+        var values = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        Console.Write("Original array ");
+
+        foreach( var element in values)
+        {
+            Console.Write($" {element}");
+        }
+
+        var filtered =
+            from value in values
+            where value > 4
+            select value;
+
+        Console.Write("\nArray values greater than 4:");
+
+        foreach(var element in filtered)
+        {
+            Console.Write($" {element}");
+        }
+
+        var sorted =
+            from value in values
+            orderby value
+            select value;
+
+        Console.Write("\nOriginal array, sorted:");
+
+        foreach (var element in sorted)
+        {
+            Console.Write($" {element}");
+        }
+
+        var sortFilteredResults =
+            from value in filtered
+            orderby value descending
+            select value;
+
+        Console.Write("\nValues greater than 4; descending order (two quaries) : ");
+
+        foreach (var element in sortFilteredResults)
+        {
+            Console.Write($" {element}");
+        }
+
+
+        var sortAndFilter =
+            from value in values
+            where value > 4
+            orderby value descending
+            select value;
+
+        Console.Write("\nValues greater than 4; descending order (one query) : ");
+
+        foreach (var element in sortAndFilter)
+        {
+            Console.Write($" {element}");
+        }
+
+    }
+}
+
+
+   
+
+ 
