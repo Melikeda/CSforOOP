668ad320baa8a68a4bfcb25fc4903fce34794355
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-04 19:55:58 +0300

    HelloAgainC#.cs

--- HelloAgainC#.cs
+++ HelloAgainC#.cs
@@ -2,7 +2,12 @@ using system;
 
 class Welcome1{
     static void Main() {  // Her bir sınıf bildirimi { ile başlar ve } ile biter
-        Console.WriteLine("Welcome the C# again!");
+        Console.WriteLine("Welcome");
+        Console.WriteLine("the C# again!");
+        
+        Console.Write("Hİ ");
+        Console.Write("I am perfect");
     } // end Main
 
 } // end clas Welcome1
+
