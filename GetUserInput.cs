c8551293d9e289733f059f8ca9aadb074d989da5
Melike Külahcı <m.edakulahci@gmail.com>
2025-10-04 21:25:26 +0300

    GetUserInput.cs

---
+++ GetUserInput.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Enter username:");
+      string userName = Console.ReadLine();
+      Console.WriteLine("Username is: " + userName);
+    }
+  }
+}
