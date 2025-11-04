6ae0b20c09a017fe82b0669eafca2dc7f3074902
Melike Külahcı <m.edakulahci@gmail.com>
2025-11-04 19:16:57 +0300

    YM_İfElseStatement.cs

---
+++ YM_İfElseStatement.cs
@@ -0,0 +1,21 @@
+/* if (koşul)
+   {
+    yapılacak işlem 1
+   }
+else
+    {
+    yapılacak işlem 2
+    }
+*/
+
+
+int grade = 75;
+
+if (grade >= 60)
+{
+    Console.WriteLine("Geçti");
+}
+else
+{
+    Console.WriteLine("Kaldı");
+}
