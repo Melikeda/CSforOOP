7301507fa837d962cea58c35e491f5ad8619d442
Melike Külahcı <m.edakulahci@gmail.com>
2025-11-05 12:05:41 +0300

    YM_IncrementDecrementOperator.cs

---
+++ YM_IncrementDecrementOperator.cs
@@ -0,0 +1,4 @@
+int y = 5;
+Console.WriteLine(--y); // Prefix: y önce 4 olur, sonra yazdırılır → 4
+Console.WriteLine(y--); // Postfix: y önce yazdırılır → 4, sonra 1 azalır → y=3
+Console.WriteLine(y);   // y artık 3
